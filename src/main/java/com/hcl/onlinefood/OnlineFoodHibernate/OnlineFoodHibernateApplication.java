044921b8153729010275eb25db7a293ebe215f9f
vaddadipranathi <84893536+vaddadipranathi@users.noreply.github.com>
2021-06-01 11:45:04 +0530

    Update OnlineFoodHibernateApplication.java

--- src/main/java/com/hcl/onlinefood/OnlineFoodHibernate/OnlineFoodHibernateApplication.java
+++ src/main/java/com/hcl/onlinefood/OnlineFoodHibernate/OnlineFoodHibernateApplication.java
@@ -3,10 +3,19 @@ package com.hcl.onlinefood.OnlineFoodHibernate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
 //@ComponentScan({ "com.hcl.onlinefood.OnlineFoodHibernate.*" })
-public class OnlineFoodHibernateApplication {
+public class OnlineFoodHibernateApplication extends SpringBootServletInitializer{
+		 @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+         return application.sources(SpringbootFoodApplication.class);
+    }
+
+
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context=SpringApplication.run(OnlineFoodHibernateApplication.class, args);
